2c33c37c1cbea39c6d498329761edac52ed694f7
Just-Goo <dabiloro13+justgo@gmail.com>
2024-01-19 13:37:11 +0100

    Initial commit

---
+++ cmd/web/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (a *application) listenAndServe() error {
+
+	host := fmt.Sprintf("%s:%s", a.server.host, a.server.port)
+
+	srv := http.Server{
+		Handler:     a.routes(),
+		Addr:        host,
+		ReadTimeout: 300 * time.Second,
+	}
+
+	a.infoLog.Printf("Listening on %s\n", host)
+
+	return srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Just-Goo/TY_Hacker_News_Clone
+
+go 1.21.5
+
+require github.com/go-chi/chi/v5 v5.0.11 // indirect
